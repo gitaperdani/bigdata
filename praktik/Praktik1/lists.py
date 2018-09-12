9fe5120ac99328bc8f3c6fa8b188fc00aac3307e
gitaperdani <gitaperdani08@gmail.com>
2018-09-12 14:38:14 +0700

    Lists

---
+++ praktik/Praktik1/lists.py
@@ -0,0 +1,26 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> squares = [1, 4, 9, 16, 25]
+>>> squares
+[1, 4, 9, 16, 25]
+>>> squares[0]
+1
+>>> squares[-1]
+25
+>>> squares[-3]
+9
+>>> squares[-3:]
+[9, 16, 25]
+>>> squares[-2:]
+[16, 25]
+>>> squares[:]
+[1, 4, 9, 16, 25]
+>>> squares + [36, 49, 64, 81, 100]
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+>>> cubes = [1, 8, 27, 65, 125]
+>>> 4 ** 3  # the cube of 4 is 64, not 65!
+64
+>>> cubes[3] = 64  # replace the wrong value
+>>> cubes
+[1, 8, 27, 64, 125]
+>>> 
