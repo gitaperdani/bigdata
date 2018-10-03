0c62dc86e3d4e06bd89aa0939dd9142b8bcdfb0b
gitaperdani <gitaperdani08@gmail.com>
2018-10-03 15:03:39 +0700

    Tambah

--- minggu-04/praktik/src/fibo.py
+++ minggu-04/praktik/src/fibo.py
@@ -13,4 +13,11 @@ def fib2(n):   # return Fibonacci series up to n
     while a < n:
         result.append(a)
         a, b = b, a+b
+
     return result
+
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
+
+    
